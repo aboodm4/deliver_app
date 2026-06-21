2b29c5106d1b0854e758566549fc377ab39ff994
Ghaith-Issa <supersonic.alpha.aurora@gmail.com>
2026-06-22 02:44:48 +0300

    update console

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Jobs\ProcessDailySalesJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('diss', function () {
+dispatch(new ProcessDailySalesJob);
+})->everyMinute();
